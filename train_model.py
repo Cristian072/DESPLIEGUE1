f39d85d267a9e90af02ea43915ccc74b582f1369
Cristian Walter <crisvi072@gmail.com>
2026-01-06 14:36:31 -0500

    despliegue 10 clusters

--- train_model.py
+++ train_model.py
@@ -192,7 +192,7 @@ def main():
         return
     
     # Entrenar modelo (usar None para encontrar k óptimo automáticamente)
-    model, scaler, score = train_model(X, n_clusters=5)
+    model, scaler, score = train_model(X, n_clusters=10)
     
     # Guardar modelo
     save_model(model, scaler, feature_cols)
